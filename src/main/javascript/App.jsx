3877bf0fec43553ded901157b1f0ce11d88a5e15
radu <radu.helstern@deskpro.com>
2018-03-21 14:41:41 +0000

    read the ticket owner's email

--- src/main/javascript/App.jsx
+++ src/main/javascript/App.jsx
@@ -13,6 +13,30 @@ export default class App extends React.Component {
     dpapp: PropTypes.object
   };
 
+  /**
+   * Returns the ticket owner's email
+   *
+   * @returns {Promise.<String, Error>}
+   */
+  readTicketOwnerEmail()
+  {
+    const { context } = this.props.dpapp;
+    return context.getTabData().then(tabData => {
+
+      const { emails, primary_email } = tabData.api_data.person;
+
+      if (primary_email && primary_email.email) {
+        return primary_email.email;
+      }
+
+      if (emails.length) {
+        return emails[0].email;
+      }
+
+      return Promise.reject(new Error('could not find the email of the ticket owner'));
+    });
+  }
+
   render() {
     return (
       <div>Hello world</div>
